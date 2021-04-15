ee537d8e76fc8ea59d9a8248986b7ed02b9b83ac
KwatME <kwat.me@icloud.com>
2021-04-15 14:58:50 -0700

    1 line

--- src/compute_zd.jl
+++ src/compute_zd.jl
@@ -1,16 +1,11 @@
 function compute_zd(v1::Vector{Float64}, v2::Vector{Float64})::Vector{Float64}
 
+    kld1 = compute_kld(v1, v2)
 
-    kld_1 = compute_kld(v1, v2)
+    kld2 = compute_kld(v2, v1)
 
-
-    kld_2 = compute_kld(v2, v1)
-
-
-    return kld_1 .- kld_2
-
+    return kld1 .- kld2
 
 end
-
 
 export compute_zd
