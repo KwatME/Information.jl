0a2573e7f559901b1b952298ac0cfbd44d51cca2
KwatME <kwat.me@icloud.com>
2021-04-18 16:06:02 -0700

    Sync

--- src/compute_zd.jl
+++ src/compute_zd.jl
@@ -3,10 +3,12 @@ function compute_zd(
     v2::Vector{Float64},
 )::Vector{Float64}
 
+    #
     kld1 = compute_kld(v1, v2)
 
     kld2 = compute_kld(v2, v1)
 
+    #
     return kld1 .- kld2
 
 end
