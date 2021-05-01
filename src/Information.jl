296976036101f0066332a9b73462270eec736d39
KwatME <kwat.me@icloud.com>
2021-04-30 23:06:34 -0700

    Again

--- src/Information.jl
+++ src/Information.jl
@@ -1,10 +1,10 @@
 module Information
 
 include("compute_ic.jl")
-include("compute_jsd1.jl")
-include("compute_jsd2.jl")
-include("compute_jsd3.jl")
-include("compute_jsd4.jl")
+include("compute_ided.jl")
+include("compute_ides.jl")
+include("compute_idrd.jl")
+include("compute_idrs.jl")
 include("compute_kld.jl")
 include("compute_mi.jl")
 include("compute_s.jl")
